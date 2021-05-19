89e425b41e9981e81575ed7c115b7016fb3e4e44
sean <sr.webcode@gmail.com>
2021-05-19 17:38:42 +0800

    chore(app): re-sync build for s3Upload plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,7 +30,7 @@ import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js
 import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import s3Upload from 'ckeditor5-sss-upload/src/s3upload'
+import s3Upload from 'ckeditor5-sss-upload/src/s3upload.js'
 
 
 const BALLOON_EDITOR_PLUGINS = [
